a759832031b6ad8f4b57b54cc17fe8baaa1fab34
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2022-02-04 18:48:39 -0500

    feat: Checklist and Control keyword functions created to run tests
    - I'm starting the convention of making a descriptive, powershell-standard-named
      function, and then providing it as a DSL word using an alias.  I think this is
      ultimately cleaner, as it allows me to describe a function in "Verb the Noun"
      syntax, but then implement it in a DSL anyway I like.
    
    - The Control function runs the tests using the `-Container` feature of Pester
      and the results are passed to the pipeline
    
    - The Checklist function executes all Controls, returning the result to the pipeline.

---
+++ source/infraspective/public/Invoke-InfraspecControl.ps1
@@ -0,0 +1,85 @@
+
+Set-Alias -Name Control -Value 'Invoke-InfraspecControl' -Description 'Execute a infraspective Control'
+
+Function Invoke-InfraspecControl {
+    <#
+.SYNOPSIS
+    A security control consisting of one or more tests and metadata about the test.
+.DESCRIPTION
+    This function is aliased by the `Control` keyword, and maps directly to the concept of a Security Control found
+    in many frameworks such as CIS, STIG, HIPAA, etc.  A control consists of one or more tests, such as the
+    existence of a file permission, or the status of a service and maps that to a recommended setting for that test
+    found in one of those frameworks, or your corporate or personal security policy.
+
+    A passing test(s) means that the system under test complies with the given control, while a failing test means
+    that the system is not in compliance
+    The tests are regular Pester tests, using the standard "Describe/Context/It/Should" keywords.  These tests are
+    passed directly to Pester (Invoke-Pester) and the results are returned.
+
+.EXAMPLE
+    ``` powershell
+    Control "xccdf_blah" -Resource "Windows" -Impact 1 -Reference 'CVE:123' {
+        Describe "cis control 123" {
+            It "Should have foo set to bar" {
+                $p.foo | Should -Be "bar"
+            }
+        }
+    }
+    ```
+    #>
+    [CmdletBinding()]
+    param(
+        # The unique ID for this control
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$Name,
+
+        # The criticality, if this control fails
+        [Parameter(
+            ValueFromPipeline
+        )]
+        [string]$Impact,
+
+        # The human readable title
+        [Parameter()]
+        [string]$Title,
+
+        # References to external tools and databases
+        [Parameter()]
+        [string[]]$Reference,
+
+        # Tags associated with this control
+        [Parameter()]
+        [string[]]
+        $Tags,
+
+        # The type of resource to test
+        [Parameter()]
+        [string]$Resource,
+
+        # An optional description of the test
+        [Parameter()]
+        [string[]]$Description,
+
+        # The tests associated with this control
+        [Parameter(Position = 1)]
+        [ValidateNotNull()]
+        [scriptblock]$Test
+    )
+    begin {
+        $config = New-PesterConfiguration @{
+            Run = @{
+                PassThru = $true
+            }
+            Output = @{
+                Verbosity = 'None'
+            }
+        }
+    }
+    process {
+        $container = New-PesterContainer -ScriptBlock $Test
+        $result = Invoke-Pester -Container $container -Output 'None' -PassThru
+    }
+    end {
+        $result
+    }
+}
