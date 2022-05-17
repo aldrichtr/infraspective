67f43079a056f3a3d72c0366b0250f6ab1ac4296
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2022-05-17 15:53:33 -0400

    bugfix: CommentBasedHelp tests read examples

--- tests/Module/CommentBasedHelp.Tests.ps1
+++ tests/Module/CommentBasedHelp.Tests.ps1
@@ -21,58 +21,69 @@ BeforeDiscovery {
             }
         }
         # Just the examples
-        $e = $h.Help.Examples.Example | ForEach-Object { @{ Example = $_ } }
-        # The AST of the function
-        $a = (Get-Content -Path "function:/$c" -ErrorAction Ignore).Ast
-        # Build a table of the help compenents we want to use
-        $cmd = @{
-            Command  = $c
-            Help     = $h
-            AST      = $a
-            Examples = $e
+        $e = $h.Examples.Example | ForEach-Object {
+            $t = $_.Title -replace '-', ''
+            @{
+                Example = @{
+                    Title        = $t
+                    Remarks      = $_.Remarks
+                    Code         = $_.Code
+                    Introduction = $_.Introduction
+                }
+            }
         }
-        # finally, add it to the array of Commands to test
-        $CommandList += $cmd
-    }
-}
-
-Describe "Comment-based help for <Command>" -Tags @('analyze', 'help') -ForEach $CommandList {
-    Context "Main help sections" {
-        It "<Command> should have help content" {
-            $Help | Should -Not -BeNullOrEmpty
-        }
-
-        It "<Command> should contain a synopsis" {
-            $Help.Synopsis | Should -Not -BeNullOrEmpty
-        }
-
-        It "<Command> should contain a description" {
-            $Help.Description | Should -Not -BeNullOrEmpty
+            # The AST of the function
+            $a = (Get-Content -Path "function:/$c" -ErrorAction Ignore).Ast
+            # Build a table of the help compenents we want to use
+            $cmd = @{
+                Command    = $c
+                Help       = $h
+                AST        = $a
+                Examples   = $e
+                Parameters = $p
+            }
+            # finally, add it to the array of Commands to test
+            $CommandList += $cmd
         }
     }
 
-    Context "Examples" {
-        It "Should have at least one usage example" {
-            $Help.Examples.Example.Code.Count | Should -BeGreaterOrEqual 1
+    Describe "Comment-based help for <Command>" -Tags @('analyze', 'help') -ForEach $CommandList {
+        Context "Main help sections" {
+            It "<Command> should have help content" {
+                $Help | Should -Not -BeNullOrEmpty
+            }
+
+            It "<Command> should contain a synopsis" {
+                $Help.Synopsis | Should -Not -BeNullOrEmpty
+            }
+
+            It "<Command> should contain a description" {
+                $Help.Description | Should -Not -BeNullOrEmpty
+            }
         }
 
-        It "lists a description for $Command example: <Title>" -TestCases $Examples {
-            $Example.Remarks | Should -Not -BeNullOrEmpty -Because "example $($Example.Title) should have a description!"
+        Context "Examples" {
+            It "Should have at least one usage example" {
+                $Examples.Count | Should -BeGreaterOrEqual 1
+            }
+
+            It "lists a description for $Command example: <Example.Title>" -TestCases $Examples {
+                $Example.Remarks | Should -Not -BeNullOrEmpty -Because "example $($Example.Title) should have a description!"
+            }
+        }
+
+
+        Context "Parameters" {
+            It "Should have parameter help available" {
+                $Parameters.Count | Should -BeGreaterThan 0
+            }
+
+            It "Should have a help entry for all parameters" {
+                $Parameters.Count | Should -Be $AST.Body.ParamBlock.Parameters.Count -Because 'the number of parameters in the help should match the number in the function script'
+            }
+
+            It "-<Name> should have a Description" -Foreach $Parameters {
+                $Description | Should -Not -BeNullOrEmpty
+            }
         }
     }
-
-
-    Context "Parameters" {
-        It "Should have parameter help available" {
-            $Parameters.Count | Should -BeGreaterThan 0
-        }
-
-        It "Should have a help entry for all parameters" {
-            $Parameters.Count | Should -Be $AST.Body.ParamBlock.Parameters.Count -Because 'the number of parameters in the help should match the number in the function script'
-        }
-
-        It "-<Name> should have a Description" -Foreach $Parameters {
-            $Description | Should -Not -BeNullOrEmpty
-        }
-    }
-}
