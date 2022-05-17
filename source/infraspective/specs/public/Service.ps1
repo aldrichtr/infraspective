fc174719a6c93b9ade4bdb2dbcf50b95dbc395f3
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2022-05-17 17:24:28 -0400

    refactor: Service.ps1 pssa errors

--- source/infraspective/specs/public/Service.ps1
+++ source/infraspective/specs/public/Service.ps1
@@ -1,40 +1,46 @@
-<#
-.SYNOPSIS
-    Test a Service.
-.DESCRIPTION
-    Test the Status of a given Service.
-.PARAMETER Target
-    Specifies the service names of service.
-.PARAMETER Should 
-    A Script Block defining a Pester Assertion.   
-.EXAMPLE
-    Service w32time { Should Be Running }
-.EXAMPLE
-    Service bits { Should Be Stopped }
-.NOTES
-    Only validates the Status property. Assertions: Be
-#>
 function Service {
-    [CmdletBinding(DefaultParameterSetName='prop')]
-    param( 
-        [Parameter(Mandatory, Position=1)]
+    <#
+    .SYNOPSIS
+        Test a Service.
+    .DESCRIPTION
+        Test the Status of a given Service.
+    .EXAMPLE
+        Service w32time { Should -Be Running }
+    .EXAMPLE
+        Service bits { Should -Be Stopped }
+    .NOTES
+        Only validates the Status property. Assertions: Be
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'prop')]
+    param(
+        # Specifies the service names of service.
+        [Parameter(Mandatory, Position = 1)]
         [Alias("Name")]
         [string]$Target,
 
-        [Parameter(Position=2, ParameterSetName='prop')]
+        # Specifies the Property of the Service to test.  Defaults to 'Status'
+        [Parameter(Position = 2, ParameterSetName = 'prop')]
         [string]$Property = 'Status',
 
-        [Parameter(Mandatory, Position=2, ParameterSetName='noprop')]
-        [Parameter(Mandatory, Position=3, ParameterSetName='prop')]
+        # A Script Block defining a Pester Assertion.
+        [Parameter(Mandatory, Position = 2, ParameterSetName = 'noprop')]
+        [Parameter(Mandatory, Position = 3, ParameterSetName = 'prop')]
         [scriptblock]$Should
     )
 
-    if (-not $PSBoundParameters.ContainsKey('Property')) {
-        $Property = 'Status'
-        $PSBoundParameters.Add('Property', $Property)
+    begin {
+    }
+    process {
+        if (-not $PSBoundParameters.ContainsKey('Property')) {
+            $Property = 'Status'
+            $PSBoundParameters.Add('Property', $Property)
+        }
+
+        $params = Get-PoshspecParam -TestName Service -TestExpression {
+            Get-Service -Name '$Target' } @PSBoundParameters
+    }
+    end {
+        Invoke-PoshspecExpression @params
     }
 
-    $params = Get-PoshspecParam -TestName Service -TestExpression {Get-Service -Name '$Target'} @PSBoundParameters
-    
-    Invoke-PoshspecExpression @params
 }
