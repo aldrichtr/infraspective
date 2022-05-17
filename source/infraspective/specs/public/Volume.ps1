5cf13fc783f605099c54198b4f701c3d292b6353
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2022-05-17 13:47:59 -0400

    refactor: Volume.ps1 pssa errors

--- source/infraspective/specs/public/Volume.ps1
+++ source/infraspective/specs/public/Volume.ps1
@@ -1,59 +1,67 @@
- <#
-.SYNOPSIS
-    Test the volume specified 
-.DESCRIPTION
-    Can be specified to target a specific volume for testing
-.PARAMETER Target
-    Specifies the drive letter or file system label of the volume to test
-.PARAMETER Property
-    Specifies an optional property to test for on the volume
-.PARAMETER Should 
-    A Script Block defining a Pester Assertion.
-.EXAMPLE
- Volume C HealthStatus { Should be 'Healthy' }
-.EXAMPLE
- Volume C FileSystem { Should be 'NTFS' }
- .EXAMPLE
- Volume D AllocationUnitSize { Should be 64K }
-.EXAMPLE
- Volume MyFileSystemLabel SizeRemaining { Should BeGreaterThan 1GB }
-.NOTES
-    Assertions: Be
-#>  
-  
+
 function Volume {
-    [CmdletBinding(DefaultParameterSetName="Default")]
+    <#
+    .SYNOPSIS
+        Test the volume specified
+    .DESCRIPTION
+        Can be specified to target a specific volume for testing
+    .EXAMPLE
+        Volume C HealthStatus { Should -Be 'Healthy' }
+    .EXAMPLE
+        Volume C FileSystem { Should -Be 'NTFS' }
+    .EXAMPLE
+        Volume D AllocationUnitSize { Should -Cbe 64K }
+    .EXAMPLE
+        Volume MyFileSystemLabel SizeRemaining { Should -BeGreaterThan 1GB }
+    .NOTES
+        Assertions: Be
+    #>
+    [CmdletBinding(DefaultParameterSetName = "Default")]
     param(
-        [Parameter(Mandatory, Position=1,ParameterSetName="Default")]
-        [Parameter(Mandatory, Position=1,ParameterSetName="Property")]
+        # Specifies the drive letter or file system label of the volume to test
+        [Parameter(Mandatory, Position = 1, ParameterSetName = "Default")]
+        [Parameter(Mandatory, Position = 1, ParameterSetName = "Property")]
         [Alias('Name')]
         [string]$Target,
-        
-        [Parameter(Position=2,ParameterSetName="Property")]
+
+        # Specifies an optional property to test for on the volume
+        [Parameter(Position = 2, ParameterSetName = "Property")]
         [ValidateSet('AllocationUnitSize', 'DedupMode', 'DriveLetter', 'DriveType', 'FileSystem', 'FileSystemLabel',
-                     'FileSystemType', 'HealthStatus', 'ObjectId', 'OperationalStatus', 'Path', 'Size', 'SizeRemaining')]
+        'FileSystemType', 'HealthStatus', 'ObjectId', 'OperationalStatus', 'Path', 'Size', 'SizeRemaining')]
         [string]$Property,
 
-        [Parameter(Mandatory, Position=2,ParameterSetName="Default")]
-        [Parameter(Mandatory, Position=3,ParameterSetName="Property")]
+        # A Script Block defining a Pester Assertion.
+        [Parameter(Mandatory, Position = 2, ParameterSetName = "Default")]
+        [Parameter(Mandatory, Position = 3, ParameterSetName = "Property")]
         [scriptblock]$Should
     )
 
-    function GetVolume([string]$Name) {
-        if ($Name.Length -eq 1) {
-            $v = Get-Volume -DriveLetter $Name -ErrorAction SilentlyContinue
-            if (-not $v) {
-                $v = Get-Volume -FileSystemLabel $Name  -ErrorAction SilentlyContinue    
+    begin {
+        function GetVolume([string]$Name) {
+            <#
+            .SYNOPSIS
+                Lookup the volume by drive letter or label
+            .DESCRIPTION
+                If the Name variable is only one character, lookup volume by driveletter, otherwise
+                by label
+            #>
+            if ($Name.Length -eq 1) {
+                $v = Get-Volume -DriveLetter $Name -ErrorAction SilentlyContinue
+                if (-not $v) {
+                    $v = Get-Volume -FileSystemLabel $Name  -ErrorAction SilentlyContinue
+                }
+            } else {
+                $v = Get-Volume -FileSystemLabel $Name  -ErrorAction SilentlyContinue
             }
-        } else {
-            $v = Get-Volume -FileSystemLabel $Name  -ErrorAction SilentlyContinue
+            $v
         }
-        $v
     }
-    
-    $expression = { GetVolume -Name '$Target' }
-                 
-    $params = Get-PoshspecParam -TestName Volume -TestExpression $expression @PSBoundParameters
-    
-    Invoke-PoshspecExpression @params
+    process {
+        $expression = { GetVolume -Name '$Target' }
+
+        $params = Get-PoshspecParam -TestName Volume -TestExpression $expression @PSBoundParameters
+    }
+    end {
+        Invoke-PoshspecExpression @params
+    }
 }
