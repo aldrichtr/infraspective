ddf256bd3c85465aff39802156b85a567bdeffee
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2022-05-17 18:07:06 -0400

    refactor: LocalGroup.ps1 pssa errors

--- source/infraspective/specs/public/LocalGroup.ps1
+++ source/infraspective/specs/public/LocalGroup.ps1
@@ -1,34 +1,37 @@
-<#
-.SYNOPSIS
-    Test if a local group exists.
-.DESCRIPTION
-    Test if a local group exists.
-.PARAMETER Target
-    The local group name to test for. Eg 'Administrators'
-.PARAMETER Should 
-    A Script Block defining a Pester Assertion.  
-.EXAMPLE
-    LocalGroup 'Administrators' { should not BeNullOrEmpty }    
-.EXAMPLE
-    LocalGroup 'BadGroup' { should BeNullOrEmpty }
-.NOTES
-    Assertions: BeNullOrEmpty
-#>
-    
 function LocalGroup {
+    <#
+    .SYNOPSIS
+        Test if a local group exists.
+    .DESCRIPTION
+        Test if a local group exists.
+    .EXAMPLE
+        LocalGroup 'Administrators' { Should -Not -BeNullOrEmpty }
+    .EXAMPLE
+        LocalGroup 'BadGroup' { Should -BeNullOrEmpty }
+    .NOTES
+        Assertions: BeNullOrEmpty
+    #>
+
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory, Position=1)]
+        # The local group name to test for. Eg 'Administrators'
+        [Parameter(Mandatory, Position = 1)]
         [Alias('Name')]
         [string]$Target,
-        
-        [Parameter(Mandatory, Position=2)]
+
+        # A Script Block defining a Pester Assertion.
+        [Parameter(Mandatory, Position = 2)]
         [scriptblock]$Should
     )
 
-    $expression = {Get-CimInstance -ClassName Win32_Group -Filter "Name = '$Target'"}
-    
-    $params = Get-PoshspecParam -TestName LocalGroup -TestExpression $expression @PSBoundParameters
-    
-    Invoke-PoshspecExpression @params
+    begin {
+    }
+    process {
+        $expression = { Get-CimInstance -ClassName Win32_Group -Filter "Name = '$Target'" }
+
+        $params = Get-PoshspecParam -TestName LocalGroup -TestExpression $expression @PSBoundParameters
+    }
+    end {
+        Invoke-PoshspecExpression @params
+    }
 }
