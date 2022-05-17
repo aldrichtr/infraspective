46c4baa50b38a7f73d22ca6378275dfa46b5a93c
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2022-05-17 13:54:40 -0400

    refactor: UserRightsAssignment.ps1 pssa errors

--- tests/infraspective/specs/UserRightsAssignment.Spec.Tests.ps1
+++ tests/infraspective/specs/UserRightsAssignment.Spec.Tests.ps1
@@ -16,7 +16,7 @@ Describe 'Testing the UserRightsAssignment specification' -Tag @('unit', 'UserRi
         It "Should return the correct test Name" {
             $results.Name | Should -Be "UserRightsAssignment 'SeNetworkLogonRight' Should -Be @(`"BUILTIN\Users`", `"BUILTIN\Administrators`")"
         }
-            
+
         It "Should return the correct test Expression" {
             $results.Expression | Should -Be "Get-AccountsWithUserRight -Right 'SeNetworkLogonRight' | Select-Object -ExpandProperty Account | Should -Be @(`"BUILTIN\Users`", `"BUILTIN\Administrators`")"
         }
