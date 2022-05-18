28e2b9776cdf9a12482367c2cc02e03b7d096fdd
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2022-05-18 11:58:05 -0400

    refactor: auditpolicy pssa error due to variable scope

--- source/infraspective/specs/public/AuditPolicy.ps1
+++ source/infraspective/specs/public/AuditPolicy.ps1
@@ -14,7 +14,7 @@ function AuditPolicy {
     .NOTES
         Assertions: Be, BeExactly, Match, MatchExactly
     #>
-    [CmdletBinding(DefaultParameterSetName="Default")]
+    [CmdletBinding(DefaultParameterSetName = "Default")]
     param(
         # Specifies the category of the Audit Policy.
         [Parameter(Mandatory, Position = 1)]
@@ -22,36 +22,36 @@ function AuditPolicy {
         [ValidateSet(
             "System", "Logon/Logoff", "Object Access", "Privilege Use",
             "Detailed Tracking", "Policy Change", "Account Management", "DS Access", "Account Logon"
-            )]
-            [string]$Qualifier,
+        )]
+        [string]$Qualifier,
 
-            # Specifies the subcategory of the Audit policy.
-            [Parameter(Mandatory, Position=2)]
-            [Alias("Subcategory")]
-            [ValidateSet(
-                "Security System Extension", "System Integrity", "IPsec Driver", "Other System Events",
-                "Security State Change", "Logon", "Logoff", "Account Lockout", "IPsec Main Mode",
-                "IPsec Quick Mode", "IPsec Extended Mode", "Special Logon", "Other Logon/Logoff Events",
-                "Network Policy Server", "User / Device Claims", "Group Membership", "File System",
-                "Registry", "Kernel Object", "SAM", "Certification Services", "Application Generated",
-                "Handle Manipulation", "File Share", "Filtering Platform Packet Drop",
-                "Filtering Platform Connection", "Other Object Access Events",
-                "Detailed File Share", "Removable Storage", "Central Policy Staging", "Sensitive Privilege Use",
-                "Non Sensitive Privilege Use", "Other Privilege Use Events", "Process Termination", "DPAPI Activity",
-                "RPC Events", "Plug and Play Events", "Token Right Adjusted Events", "Process Creation",
-                "Audit Policy Change", "Authentication Policy Change", "Authorization Policy Change",
-                "MPSSVC Rule-Level Policy Change", "Filtering Platform Policy Change",
-                "Other Policy Change Events", "User Account Management", "Computer Account Management",
-                "Security Group Management", "Distribution Group Management", "Application Group Management",
+        # Specifies the subcategory of the Audit policy.
+        [Parameter(Mandatory, Position = 2)]
+        [Alias("Subcategory")]
+        [ValidateSet(
+            "Security System Extension", "System Integrity", "IPsec Driver", "Other System Events",
+            "Security State Change", "Logon", "Logoff", "Account Lockout", "IPsec Main Mode",
+            "IPsec Quick Mode", "IPsec Extended Mode", "Special Logon", "Other Logon/Logoff Events",
+            "Network Policy Server", "User / Device Claims", "Group Membership", "File System",
+            "Registry", "Kernel Object", "SAM", "Certification Services", "Application Generated",
+            "Handle Manipulation", "File Share", "Filtering Platform Packet Drop",
+            "Filtering Platform Connection", "Other Object Access Events",
+            "Detailed File Share", "Removable Storage", "Central Policy Staging", "Sensitive Privilege Use",
+            "Non Sensitive Privilege Use", "Other Privilege Use Events", "Process Termination", "DPAPI Activity",
+            "RPC Events", "Plug and Play Events", "Token Right Adjusted Events", "Process Creation",
+            "Audit Policy Change", "Authentication Policy Change", "Authorization Policy Change",
+            "MPSSVC Rule-Level Policy Change", "Filtering Platform Policy Change",
+            "Other Policy Change Events", "User Account Management", "Computer Account Management",
+            "Security Group Management", "Distribution Group Management", "Application Group Management",
             "Other Account Management Events", "Directory Service Changes", "Directory Service Replication",
             "Detailed Directory Service Replication", "Directory Service Access",
             "Kerberos Service Ticket Operations", "Other Account Logon Events",
             "Kerberos Authentication Service", "Credential Validation"
-            )]
-            [string]$Target,
+        )]
+        [string]$Target,
 
-            # A Script Block defining a Pester Assertion.
-            [Parameter(Mandatory, Position=3)]
+        # A Script Block defining a Pester Assertion.
+        [Parameter(Mandatory, Position = 3)]
         [scriptblock]$Should
     )
     begin {
@@ -73,25 +73,30 @@ function AuditPolicy {
                 [Parameter( Mandatory = $true
                 )]
                 [string]$Subcategory
-                )
-                begin {
-                }
-                process {
-                    if (Test-RunAsAdmin){
-                        $policy = auditpol /get /category:$Category |
-                            Where-Object -FilterScript {$_ -match "^\s+$Subcategory"} |
-                                ForEach-Object -Process {($_.trim() -split "\s{2,}")[1]}
-                    } else {
-                        throw "You must run as Administrator to test AuditPolicy"
+            )
+            begin {
+            }
+            process {
+                $sc_match = "^\s+$Subcategory"
+                if (Test-RunAsAdmin) {
+                    $policy = auditpol /get /category:$Category |
+                    Where-Object {
+                        $_ -match $sc_match
+                    } |
+                    ForEach-Object -Process {
+                                    ($_.trim() -split "\s{2,}")[1]
                     }
+                } else {
+                    throw "You must run as Administrator to test AuditPolicy"
                 }
-                end {
-                    $policy
-                }
+            }
+            end {
+                $policy
+            }
         }
     }
     process {
-        $expression = {GetAuditPolicy -Category '$Qualifier' -Subcategory '$Target'}
+        $expression = { GetAuditPolicy -Category '$Qualifier' -Subcategory '$Target' }
         $params = Get-PoshspecParam -TestName AuditPolicy -TestExpression $expression @PSBoundParameters
     }
     end {
