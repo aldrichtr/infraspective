f8022d4970eecac46dc826e8d209a080c84d5e40
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2022-07-22 18:45:31 -0400

    doc: WIP modified help to support testing PSDocs

--- source/infraspective/specs/public/AppPool.ps1
+++ source/infraspective/specs/public/AppPool.ps1
@@ -4,43 +4,95 @@ function AppPool {
     .SYNOPSIS
         Test an Application Pool
     .DESCRIPTION
-        Used To Determine if Application Pool is Running and Validate Various Properties
+        `AppPool` will test IIS Application Pool status and validate various properties.  Nested properties are
+        supported and can be accessed by using a '.' as the separator such as `property.subproperty`
+
     .EXAMPLE
-        AppPool TestSite { Should -Be Started }
+        AppPool TestSite { Should -Be Started } | Foreach-Object {
+            "Hello second line of code"
+        }
+
+        ```Output
+        true
+        ```
+
+        Title: Test the status of the AppPool
+
+        This is additional text about example number 1
+
+        But this is even more text about example number 1, and there should be a blank line before
+        it, but not in the middle here
     .EXAMPLE
+        Test a property of the AppPool
+
         AppPool TestSite ManagedPipelineMode { Should -Be 'Integrated' }
+
     .EXAMPLE
         AppPool TestSite ProcessModel.IdentityType { Should -Be 'ApplicationPoolIdentity'}
+
+        Title: Test a nested property of the AppPool
+
     .NOTES
         Assertions: Be
+
+    .LINK
+        A related link
+    .FUNCTIONALITY
+        Testing,WebServer
+    .COMPONENT
+        Specification
+
     #>
-    [CmdletBinding(DefaultParameterSetName="Default")]
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("Measure-RequiresModules\Measure-RequiresModules", "")]
+    [CmdletBinding(
+        DefaultParameterSetName = 'Default'
+    )]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        'Measure-RequiresModules\Measure-RequiresModules', ''
+    )]
 
     param(
         # The name of the App Pool to be Tested
-        [Parameter(Mandatory, Position=1, ParameterSetName="Default")]
-        [Parameter(Mandatory, Position=1, ParameterSetName="Property")]
-        [Alias("Path")]
+        [Parameter(
+            ParameterSetName='Default',
+            Mandatory,
+            Position = 0
+            )]
+        [Parameter(
+            ParameterSetName='Property',
+            Mandatory,
+            Position = 0
+        )]
+        [Alias('Path')]
         [string]$Target,
 
         # The Property to be expanded. If Ommitted, Property Will Default to Status.
         # Can handle nested objects within properties
-        [Parameter(Position=2, ParameterSetName="Property")]
+        [Parameter(
+            ParameterSetName = 'Property',
+            Position = 1
+        )]
         [string]$Property,
 
         # A Script Block defining a Pester Assertion.
-        [Parameter(Mandatory, Position=2, ParameterSetName="Default")]
-        [Parameter(Mandatory, Position=3, ParameterSetName="Property")]
+        [Parameter(
+            ParameterSetName='Default',
+            Mandatory,
+            Position = 1
+        )]
+        [Parameter(
+            ParameterSetName='Property',
+            Mandatory,
+            Position = 2
+        )]
         [scriptblock]$Should
     )
     begin {
-    $IISAdmin = Get-Module -Name 'IISAdministration'
-    if ($IISAdmin) {
-      Import-Module IISAdministration
-    } else {
-      function Get-IISSite {
-        <#
+        $IISAdmin = Get-Module -Name 'IISAdministration'
+        if ($IISAdmin) {
+            Import-Module IISAdministration
+        } else {
+            function Get-IISSite {
+                <#
         .SYNOPSIS
             Get the IIS Site from the localhost using the ServerManager
         .DESCRIPTION
@@ -50,54 +102,54 @@ function AppPool {
         .EXAMPLE
             Get-IISSite -Name "default"
         #>
-        [CmdletBinding()]
-        param(
-          [Parameter(Mandatory = $true,
-            Position = 1)]
-          [string]$Name
-        )
+                [CmdletBinding()]
+                param(
+                    [Parameter(Mandatory = $true,
+                        Position = 0)]
+                    [string]$Name
+                )
 
-        begin {
-          [System.Reflection.Assembly]::LoadFrom("$($Env:windir)\system32\inetsrv\Microsoft.Web.Administration.dll") | Out-Null
-          $ServerManager = [Microsoft.Web.Administration.ServerManager]::OpenRemote("localhost")
+                begin {
+                    [System.Reflection.Assembly]::LoadFrom("$($Env:windir)\system32\inetsrv\Microsoft.Web.Administration.dll") | Out-Null
+                    $ServerManager = [Microsoft.Web.Administration.ServerManager]::OpenRemote('localhost')
+                }
+
+                process {
+                    try {
+                        Write-Verbose "Getting site $Name"
+                        Write-Output $ServerManager.Sites[$Name]
+                    }
+
+                    catch {
+                        Write-Warning $Error[0]
+                    }
+                }
+
+                end {
+                    $ServerManager.Dispose()
+                }
+            }
         }
-
-        process {
-          try {
-            Write-Verbose "Getting site $Name"
-            Write-Output $ServerManager.Sites[$Name]
-          }
-
-          catch {
-            Write-Warning $Error[0]
-          }
-        }
-
-        end {
-          $ServerManager.Dispose()
-        }
-      }
-    }
-    $expression = $null
-    $params = $null
+        $expression = $null
+        $params = $null
 
     }
     process {
         if (-not $PSBoundParameters.ContainsKey('Property')) {
-          $Property = 'State'
-          $PSBoundParameters.add('Property', $Property)
-          $expression = { Get-IISAppPool -Name '$Target' -ErrorAction SilentlyContinue }
-          $params = Get-PoshspecParam -TestName AppPool -TestExpression $expression @PSBoundParameters
+            $Property = 'State'
+            $PSBoundParameters.add('Property', $Property)
+            $expression = { Get-IISAppPool -Name '$Target' -ErrorAction SilentlyContinue }
+            $params = Get-PoshspecParam -TestName AppPool -TestExpression $expression @PSBoundParameters
         }
 
         if ($Property -like '*.*' -or $Property -like '*(*' -or $Property -like '*)*') {
-          $expression = { Get-IISAppPool -Name '$Target' -ErrorAction SilentlyContinue }
-          $params = Get-PoshspecParam -TestName AppPool -TestExpression $expression -Target $Target -Should $Should -PropertyExpression $Property
+            $expression = { Get-IISAppPool -Name '$Target' -ErrorAction SilentlyContinue }
+            $params = Get-PoshspecParam -TestName AppPool -TestExpression $expression -Target $Target -Should $Should -PropertyExpression $Property
         }
 
         else {
-          $expression = { Get-IISAppPool -Name '$Target' -ErrorAction SilentlyContinue }
-          $params = Get-PoshspecParam -TestName AppPool -TestExpression $expression @PSBoundParameters
+            $expression = { Get-IISAppPool -Name '$Target' -ErrorAction SilentlyContinue }
+            $params = Get-PoshspecParam -TestName AppPool -TestExpression $expression @PSBoundParameters
         }
     }
     end {
